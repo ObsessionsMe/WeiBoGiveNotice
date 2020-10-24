bb2dbc511d3821d7dde637f330e5899bfc719917
peng.tian <475777922@qq.com>
2020-10-24 21:29:51 +0800

    增加微博用户客户端功能对象

--- WeiBoGiveNotice/Form1.cs
+++ WeiBoGiveNotice/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,15 +19,23 @@ namespace WeiBoGiveNotice
             InitializeComponent();
         }
 
+
+        private WeBoUserClient weBoUserClient;
+
         //初始化
         private void Form1_Load(object sender, EventArgs e)
         {
             try
             {
+                //注册微博用户客户端
+                weBoUserClient = new WeBoUserClient();
+                weBoUserClient.qrCodeImageChange=new WeBoUserClient.QrCodeLoginImageChange(QrCodeLoginImageChange);
+
                 //显示图片
-                Image img = Image.FromFile(@"E:\PerTerProject\WeiBoGiveNotice\WeiBoGiveNotice\timg.jpg");
-                userPhoto.Image = img;
+                //Image img = Image.FromFile(@"E:\PerTerProject\WeiBoGiveNotice\WeiBoGiveNotice\timg.jpg");
+                //userPhoto.Image = img;
                 //第一步: 监听二维码是否有扫码操作，获取到微博账号基础信息后在窗体上展示
+                weBoUserClient.StartQrcodeLogin();
                 //第二步: 
             }
             catch (Exception ex)
@@ -33,6 +43,12 @@ namespace WeiBoGiveNotice
                 throw ex;
             }
         }
+
+        private  void QrCodeLoginImageChange(string imageUrl)
+        {
+            userPhoto.Load(imageUrl); 
+        }
+
 
         //点击开始(向前打招呼)
         private void button1_Click(object sender, EventArgs e)
